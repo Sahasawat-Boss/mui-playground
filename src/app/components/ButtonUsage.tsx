5aa1d1850d84a9bedf6d247aa224f07f0d6da05d
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-08 07:22:26 +0700

    a mui comps

--- src/app/components/ButtonUsage.tsx
+++ src/app/components/ButtonUsage.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
+import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function ButtonUsage() {
-    return <Button variant="contained">Hello world</Button>;
+export default function BasicButtons() {
+    return (
+        <div className='my-4'>
+            <h5>Button MUI</h5>
+            <div className='flex flex-col w-fit gap-2 h-40 overflow-auto'>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="contained" disabled>
+                    Disabled
+                </Button>
+                <Button variant="contained" href="#contained-buttons">
+                    Link
+                </Button>
+                <Button color="secondary">Secondary</Button>
+                <Button variant="contained" color="success">
+                    Success
+                </Button>
+                <Button variant="outlined" color="error">
+                    Error
+                </Button>
+            </div>
+        </div>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import BasicPopover from "./components/Popover"
 import ButtonUsage from "./components/ButtonUsage"
+import BasicModal from "./components/Modal"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Test</h1>
-      <ButtonUsage />
+    <div className="max-w-96 mx-auto bg-gray-100">
+      <h1 className="text-3xl text-center">Test MUI</h1>
+      <main className="flex flex-col justify-center items-center">
+        <ButtonUsage />
+        <BasicModal />
+        <BasicPopover/>
+      </main>
     </div>
   )
 }
